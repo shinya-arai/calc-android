734732eef470fa94b696da0d04e01996ccf61cc7
Arai Shinya <>
2019-06-13 21:30:49 +0900

    make calcurator

--- app/src/main/java/com/ssnyarii/tipcalc/MainActivity.kt
+++ app/src/main/java/com/ssnyarii/tipcalc/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ssnyarii.tipcalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        infoTextView.visibility = View.INVISIBLE
+
+        calcurateButton.setOnClickListener {
+            // Bill
+            val bill = billEditText.text.toString().toDouble()
+            // Tip Percentage
+            val tipPercentage = tipPercentageEditText.text.toString().toDouble()
+            // Tip
+            val tip = bill * tipPercentage / 100
+            // Total
+            val total = bill + tip
+
+            infoTextView.visibility = View.VISIBLE
+            // set info text
+            infoTextView.text = "Tip: ${doubleToDollar(tip)} Total: ${doubleToDollar(total)}"
+        }
+
+    }
+
+    private fun doubleToDollar(number: Double): String {
+        return String.format("$ %.1f", number)
     }
 }
